e310c0a79b9186ecf927e0b1686a6fa7344b56cb
Nevercome <sunss1903@163.com>
2019-06-17 21:06:49 +0800

    init

---
+++ src/test/java/com/nevercome/icuriosity/IcuriosityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nevercome.icuriosity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IcuriosityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
